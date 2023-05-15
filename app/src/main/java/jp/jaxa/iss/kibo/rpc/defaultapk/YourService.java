6f03beb93003a0fb3d94d513f49efb9807394bbf
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2023-05-15 22:03:31 +0900

    移動系を自作メソッドに置き換え

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -1,5 +1,7 @@
 package jp.jaxa.iss.kibo.rpc.defaultapk;
 
+import android.util.Log;
+
 import com.stellarcoders.CheckPoints;
 import com.stellarcoders.ConstPoints;
 import com.stellarcoders.ConstQuaternions;
@@ -24,7 +26,8 @@ public class YourService extends KiboRpcService {
         api.startMission();
 
         // move to initial position
-        api.moveTo(new Point(10.5,-9.6,4.8 ),new Quaternion(0,0,0,1),true);
+        //api.moveTo(new Point(10.5,-9.6,4.8 ),new Quaternion(0,0,0,1),true);
+        this.moveDijkstra(new Point(10.5,-9.6,4.8 ),new Quaternion(0,0,0,1));
         ConstPoints pointData = new ConstPoints();
         ConstQuaternions quaternions = new ConstQuaternions();
         List<Integer> activeTargets = api.getActiveTargets();
@@ -33,18 +36,39 @@ public class YourService extends KiboRpcService {
             activeTargets.set(i,activeTargets.get(i) - 1);
         }
 
+
+        //new Point(11.0,-9.5,5.0)
+        //api.moveTo(pointData.points.get(0),quaternions.points.get(0),true);
+        this.moveDijkstra(new Point(11.0,-9.5,5.0));
+
+        api.notifyGoingToGoal();
+        //api.moveTo(pointData.goal,quaternions.goal,true);
+        this.moveDijkstra(pointData.goal,quaternions.goal);
+        api.reportMissionCompletion("hoge");
+    }
+
+    void moveDijkstra(Point goal) {
+        CheckPoints checkPoints = new CheckPoints();
         Dijkstra3D dijManager = new Dijkstra3D();
-        Stack<PointI> move_oder = dijManager.dijkstra(checkPoints.Point2I(api.getRobotKinematics().getPosition()), checkPoints.Point2I(new Point(11.0,-9.5,5.0)));
+        Stack<PointI> move_oder = dijManager.dijkstra(checkPoints.Point2I(api.getRobotKinematics().getPosition()), checkPoints.Point2I(goal));
         while(!move_oder.empty()){
             PointI p = move_oder.pop();
             Point to = checkPoints.idx2Point(p.getX(), p.getY(), p.getZ());
             Result result = api.moveTo(to, new Quaternion(0, 0, 0, 1),true);
         }
-        //api.moveTo(pointData.points.get(0),quaternions.points.get(0),true);
 
-        api.notifyGoingToGoal();
-        api.moveTo(pointData.goal,quaternions.goal,true);
-        api.reportMissionCompletion("hoge");
+        Log.i("StellarCoders",String.format("Moved to Point is %.3f,%3.f,%.3f",goal.getX(),goal.getY(),goal.getZ()));
+    }
+    void moveDijkstra(Point goal, Quaternion q) {
+        CheckPoints checkPoints = new CheckPoints();
+        Dijkstra3D dijManager = new Dijkstra3D();
+        Stack<PointI> move_oder = dijManager.dijkstra(checkPoints.Point2I(api.getRobotKinematics().getPosition()), checkPoints.Point2I(goal));
+        while(!move_oder.empty()){
+            PointI p = move_oder.pop();
+            Point to = checkPoints.idx2Point(p.getX(), p.getY(), p.getZ());
+            Result result = api.moveTo(to, q,true);
+        }
+        Log.i("StellarCoders",String.format("Moved to Point is %.3f,%3.f,%.3f",goal.getX(),goal.getY(),goal.getZ()));
     }
 
     @Override
