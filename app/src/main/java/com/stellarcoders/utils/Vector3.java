9dbb8bb8c039db1266e12a657969f103c50ebc10
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2023-05-29 10:48:20 +0900

    外積の定義間違えていたのでfix

--- app/src/main/java/com/stellarcoders/utils/Vector3.java
+++ app/src/main/java/com/stellarcoders/utils/Vector3.java
@@ -1,6 +1,7 @@
 package com.stellarcoders.utils;
 
 import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
 
 public class Vector3 {
     private double x;
@@ -24,12 +25,21 @@ public class Vector3 {
         return new Vector3(this.x - v.x,this.y - v.y,this.z - v.y);
     }
 
+    public Vector3 add(Vector3 v){
+        return new Vector3(this.x + v.x, this.y + v.y, this.z + v.z);
+    }
+
+    public Vector3 prod(double d){
+        return new Vector3(d * this.x,d * this.y,d * this.z);
+    }
+
+
     public double dot(Vector3 v){
         return this.x * v.x + this.y * v.y + this.z * v.z;
     }
 
-    public Vector3 prod(Vector3 v){
-        return new Vector3(this.y*this.z - this.z*v.y,this.z*v.x - this.x*v.z,this.x*v.y - this.y*v.x);
+    public Vector3 cross(Vector3 v){
+        return new Vector3(this.y * v.z - this.z * v.y,this.z * v.x - this.x * v.z,this.x * v.y - this.y * v.x);
     }
 
     public double norm(){
@@ -39,4 +49,11 @@ public class Vector3 {
     public Vector3 normalize(){
         return new Vector3(x / norm(), y/ norm(), z/norm());
     }
+
+    public Vector3 rotate(Quaternion q){
+        Quaternion p = new Quaternion((float) this.x,(float) this.y,(float) this.z,0);
+        Quaternion rotated = Utils.quaternionProd(p,q);
+        return new Vector3(rotated.getX(),rotated.getY(),rotated.getZ());
+    }
+
 }
